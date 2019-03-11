f62e5a68b4d273bc1060b9a6c703c3af132595c4
Saikat <saikats_5@yahoo.co.in>
2019-03-11 09:44:35 +0530

    server post

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -2,11 +2,29 @@
 //const fs = require("fs");
 //fs.copyFileSync("file1.txt", "file2.txt");
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(request, response){
-    response.send("<h1>Hello World</h1>");
-})
+    response.sendFile(__dirname + '/index.html');
+    //console.log(__dirname);
+    //.send("<h1>Hello World</h1>");
+});
+
+app.post("/", function(req, res){
+    //console.log(req.body.num1);
+    var num1 = Number(req.body.num1); // default go as string has to convert to Number
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("Thanks " + result);
+});
+
+app.get("/contact", function(req, res){
+    res.send("<h1>Contact me</h1>");
+    //res.sendFile("index.html");
+});
 
 app.listen(3000, function(){
     console.log("Server has started on Port 3000");
